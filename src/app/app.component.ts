dd06e7db5d42c8588b380bebab1210f5f16e41aa
llladno <man30968@gmail.com>
2024-02-09 22:20:28 +0300

    main functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import axios from "axios";
+import data from './../assets/data.json'
+import {last} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-task';
+  photos: any
+  slide:number = 1
+
+  ngOnInit() {
+    this.getData()
+  }
+
+  async getData () {
+    this.photos = data.photos
+  }
+
+  rightSlide(event:any) {
+    let slider:any = document.getElementsByClassName('moveSlider')[0]
+    let photo:any = document.getElementsByClassName('photo')[0]
+    if (this.slide <= this.photos.length) {
+      photo.style.opacity = '0'
+      setTimeout(()=>{
+        this.slide ++
+        photo.style.opacity = '1'
+
+        if (this.slide >= this.photos.length){
+          this.slide = this.photos.length
+          slider.style.left = `${-40*this.slide+40}px`
+        }
+
+      },300)
+      slider.style.left = `${-40*this.slide}px`
+    }
+  }
+
+  leftSlide(event:any) {
+    let slider:any = document.getElementsByClassName('moveSlider')[0]
+    let photo:any = document.getElementsByClassName('photo')[0]
+      photo.style.opacity = '0'
+      setTimeout(() => {
+        let last = +slider.style.left.slice(0, -2)
+        this.slide--
+        photo.style.opacity = '1'
+        slider.style.left = `${last + 40}px`
+        if (last+40 > 0) {
+          slider.style.left = 0
+          this.slide = 1
+        }
+        console.log(last)
+      }, 300)
+  }
 }
+
