f3a4f58c78c1fc41a73a44516bf0ededd72efec5
llladno <man30968@gmail.com>
2024-02-10 23:53:56 +0300

    change arrows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,12 @@ export class AppComponent implements OnInit{
   title = 'angular-task';
   photos: any
   slide:number = 1
+  state = {
+    slidePlus: true,
+    slideCons: false,
+    sliderPlus: false,
+    sliderCons: true
+  }
 
   ngOnInit() {
     this.getData()
@@ -21,22 +27,27 @@ export class AppComponent implements OnInit{
     this.photos = data.photos
   }
 
+  check(){
+    console.log('log')
+  }
+
   rightSlide(event:any) {
     let slider:any = document.getElementsByClassName('moveSlider')[0]
     let photo:any = document.getElementsByClassName('photo')[0]
+    this.state.sliderCons = false
     if (this.slide <= this.photos.length) {
       photo.style.opacity = '0'
       setTimeout(()=>{
         this.slide ++
         photo.style.opacity = '1'
 
-        if (this.slide >= this.photos.length){
-          this.slide = this.photos.length
-          slider.style.left = `${-40*this.slide+40}px`
-        }
-
       },300)
-      slider.style.left = `${-40*this.slide}px`
+      let last = +slider.style.left.slice(0, -2) * -1
+      if (!(last+ 5*120 >= 840)) {
+        slider.style.left = `${-120*this.slide}px`
+      } else {
+        this.state.sliderPlus = true
+      }
     }
   }
 
@@ -48,13 +59,40 @@ export class AppComponent implements OnInit{
         let last = +slider.style.left.slice(0, -2)
         this.slide--
         photo.style.opacity = '1'
-        slider.style.left = `${last + 40}px`
-        if (last+40 > 0) {
+          slider.style.left = `${last + 120}px`
+        if (last+120 > 0) {
           slider.style.left = 0
           this.slide = 1
         }
-        console.log(last)
       }, 300)
+
+    this.state.sliderPlus = false
   }
+
+  leftSlider(event:any){
+    this.state.sliderPlus = false
+
+    let slider:any = document.getElementsByClassName('moveSlider')[0]
+    let last = +slider.style.left.slice(0, -2)
+    slider.style.left = `${last + 120}px`
+    if (last+120 > 0) {
+      slider.style.left = 0
+    } else if(last +120*2 > 0 )this.state.sliderCons = true
+    else this.state.slideCons = false
+  }
+
+  rightSlider(event:any){
+    let slider:any = document.getElementsByClassName('moveSlider')[0]
+    let last = +slider.style.left.slice(0, -2)
+    this.state.sliderCons = false
+    if (!(last * -1 + 5*120 >= 840)) {
+      slider.style.left = `${-120+last}px`
+      if (last * -1 + 5*120 >= 720){
+        this.state.sliderPlus = true
+      }
+    }
+  }
+
+
 }
 
